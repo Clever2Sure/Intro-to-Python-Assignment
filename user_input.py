0cd7cd20a172d20ca7cb648c678213fde0b32a86
Clever2Sure <cleverpaul2sure@gmail.com>
2024-11-11 20:05:01 +0100

    updated assignment

--- user_input.py
+++
@@ -1,5 +0,0 @@
-name = input("Please enter your name: ")
-age = input("Please enter your age: ")
-location = input("Please enter your location: ")
-
-print(f"Hello {name}, you are {age} years old and you live in {location}. ")
